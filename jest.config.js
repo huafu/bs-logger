4b480a0957e9221fa778bc4646c4cbb1a903ff78
Huafu Gandon <huafu.gandon@gmail.com>
2018-09-01 20:55:23 +0200

    chore(packages): update ts-jest to @next

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  transform: {
-    '\\.tsx?$': 'ts-jest',
-  },
-  testMatch: ['**/__tests__/**/*.{t,j}s?(x)', '**/?(*.)(spec|test).{t,j}s?(x)'],
-  testPathIgnorePatterns: ['<rootDir>/(node_modules|lib|es|dist)'],
-  collectCoverageFrom: ['src/**/*.{t,j}s?(x)', '!src/**/*.d.ts'],
-  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
-  globals: {
-    'ts-jest': {},
-  },
+  preset: 'ts-jest',
   testEnvironment: 'node',
 };
